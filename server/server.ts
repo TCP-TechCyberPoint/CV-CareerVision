5afe720caff509c44b936b654841fcf2302a31b5
Lior SIlman <79502469+LiorSil@users.noreply.github.com>
2025-05-28 20:45:09 +0300

    adding jwt-auth (#3)
    Co-authored-by: Itay4210 <itcoe421@gmail.com>

---
+++ server/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
+
+
+mongoose.connect(process.env.MONGO_URI || "", {})
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+  });
