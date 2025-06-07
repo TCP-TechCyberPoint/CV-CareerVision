2721b8609548085e29bd38bd774301f706569d9f
Lior Silman <lior.silmansoad@gmail.com>
2025-06-07 22:20:09 +0300

    Refactor ESLint configuration to support TypeScript, including parser integration and updated rules.

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,26 +3,36 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import * as tseslint from "typescript-eslint/eslint-config";
+import parser from "@typescript-eslint/parser";
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+const cleanedGlobals = {
+  ...globals.browser,
+  ...globals.node,
+};
+
+
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
+    ignores: ["dist"],
     languageOptions: {
+      parser,
       ecmaVersion: 2020,
+      sourceType: "module",
       globals: cleanedGlobals,
     },
     plugins: {
+      "@typescript-eslint": tseslint,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
+      ...tseslint.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
     },
-  }
-);
+  },
+];
